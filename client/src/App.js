a6c3c65500db90a6fa691c6f166073cbfa33a65f
x4nt <addisonshaw93@gmail.com>
2019-02-06 20:04:37 -0600

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Home from './components/Home';
+import Whatever from './components/Whatever';
+import './App.css';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+const App = (props) => {
+  const { classes } = props;
+  return (
+    <Router>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" className={classes.grow}>
+              Nova
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/whatever" component={Whatever} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
---
+++ client/src/components/Whatever.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Whatever extends Component {
+  render() {
+    return (
+      <div>
+        Whatever Brah!
+      </div>
+    )
+  }
+}
+
+export default Whatever;
