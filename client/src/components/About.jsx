67715ac8f740bfeb65e5e428352b7b05433f253d
x4nt <addisonshaw93@gmail.com>
2019-02-10 10:08:13 -0600

    Making the About page dynamic.

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,57 +1,56 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+import request from 'superagent';
 
-const About = () => (
-  <div>
-    <secion className="hero is-light is-fullheight">
-      <div className="hero-body">
-        <div className="container has-text-centered">
-          <h1 className="title">One</h1>
-        </div>
+class About extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.numToColor = {
+      1: 'is-light',
+      3: 'is-warning',
+      7: 'is-info',
+      13: 'is-success'
+    };
+    this.state = {
+      nums: []
+    };
+  }
+
+  componentDidMount() {
+    request
+      .get('/api/ids')
+      .set('accept', 'json')
+      .then(res => {
+        this.setState({ nums: res.body });
+      });
+  }
+
+  render() {
+    const { numToColor } = this;
+    const { nums } = this.state;
+
+    return (
+      <div className="container">
+        {nums.length === 0 && (
+          <secion className="hero is-light is-fullheight">
+            <div className="hero-body">
+              <div className="container has-text-centered">
+                <h1 className="title">Nothing to see...</h1>
+              </div>
+            </div>
+          </secion>
+        )}
+        {nums.map(num => (
+          <secion className={`hero ${numToColor[num] ? numToColor[num] : 'is-dark'} is-fullheight`}>
+            <div className="hero-body">
+              <div className="container has-text-centered">
+                <h1 className="title">{num}</h1>
+              </div>
+            </div>
+          </secion>
+        ))}
       </div>
-    </secion>
-    <section className="hero is-dark is-fullheight">
-      <div className="hero-body">
-        <div className="container has-text-centered">
-          <h1 className="title">Two</h1>
-        </div>
-      </div>
-    </section>
-    <secion className="hero is-light is-fullheight">
-      <div className="hero-body">
-        <div className="container has-text-centered">
-          <h1 className="title">Three</h1>
-        </div>
-      </div>
-    </secion>
-    <secion className="hero is-info is-fullheight">
-      <div className="hero-body">
-        <div className="container has-text-centered">
-          <h1 className="title">Three</h1>
-        </div>
-      </div>
-    </secion>
-    <secion className="hero is-light is-fullheight">
-      <div className="hero-body">
-        <div className="container has-text-centered">
-          <h1 className="title">Three</h1>
-        </div>
-      </div>
-    </secion>
-    <secion className="hero is-info is-fullheight">
-      <div className="hero-body">
-        <div className="container has-text-centered">
-          <h1 className="title">Three</h1>
-        </div>
-      </div>
-    </secion>
-    <secion className="hero is-light is-fullheight">
-      <div className="hero-body">
-        <div className="container has-text-centered">
-          <h1 className="title">Three</h1>
-        </div>
-      </div>
-    </secion>
-  </div>
-);
+    );
+  }
+}
 
 export default About;
